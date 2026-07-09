6acdc1dc336e51bf788e9f4e5e06a60bbac2fdf3
gambaboy0 <gambaboy0@gmail.com>
2026-07-09 14:01:36 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -165,7 +165,7 @@
 
   function renderNavGrids() {
     const html = buildNavGridHtml();
-    ["#nav-grid-top", "#nav-grid-profile", "#nav-grid-history", "#nav-grid-our-history", "#nav-grid-seating"].forEach((sel) => {
+    ["#nav-grid-top", "#nav-grid-profile", "#nav-grid-history", "#nav-grid-our-history", "#nav-grid-seating", "#nav-grid-menu"].forEach((sel) => {
       const el = $(sel);
       if (el) el.innerHTML = html;
     });
